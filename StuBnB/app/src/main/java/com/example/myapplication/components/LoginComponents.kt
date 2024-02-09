74519ff359436ad242b6c93935d630de3383d5fa
Juliette Xu <j556xu@uwaterloo.ca>
2024-02-09 11:12:14 -0500

    clean up code

--- StuBnB/app/src/main/java/com/example/myapplication/components/LoginComponents.kt
+++ StuBnB/app/src/main/java/com/example/myapplication/components/LoginComponents.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.components
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
