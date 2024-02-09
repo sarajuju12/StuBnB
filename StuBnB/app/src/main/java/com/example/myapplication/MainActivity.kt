59a676ceaf58381d0691bb3cdf91961e8515224c
Juliette Xu <j556xu@uwaterloo.ca>
2024-02-09 11:02:31 -0500

    re-added working dependencies for firebase

--- StuBnB/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ StuBnB/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.routers.Entry
 import com.example.myapplication.routers.Router
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
