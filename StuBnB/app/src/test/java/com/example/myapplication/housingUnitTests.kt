f093f472910f3be3770eefa82f787c3dacdb0755
michaelUWCS <hh.michael2000@gmail.com>
2024-02-13 20:55:20 -0500

    finished unit test for housing

--- StuBnB/app/src/test/java/com/example/myapplication/housingUnitTests.kt
+++ StuBnB/app/src/test/java/com/example/myapplication/housingUnitTests.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
+import com.example.myapplication.data.HousingRepository
 import com.example.myapplication.models.Date
 import org.junit.Test
-import com.example.myapplication.models.*
+import com.example.myapplication.models.Housing
+import com.example.myapplication.screens.HousingScreen
 
 import org.junit.Assert.*
 
@@ -20,14 +22,9 @@ import org.junit.Assert.*
  *
  */
 class HousingUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
 
     val startDate = Date(2019, 1, 28)
     val endDate = Date(2024, 2, 4)
-
     val house = Housing(
         name = "House",
         description = "Description for House",
@@ -40,8 +37,29 @@ class HousingUnitTest {
     )
 
     @Test
-    fun testHousing() {
-        assertEquals(house.seller, "cs346") // just run current file
+    fun testHousingAndDate() {
+        assertEquals(house.seller, "cs346")                         // testing field value
+        assertEquals(house.startDate.toString(), "2019/1/28")
+        assertEquals(house.endDate.toString(), "2024/2/4")          // testing the date toString() function override
+
     }
 
+
+    val repo = HousingRepository();
+    val housingList =  repo.getHousing();  // need a value to hold the result
+    @Test
+    fun testHousingRepo1() {
+        assertEquals(housingList[0].name, "House")
+        assertEquals(housingList[0].description, "Description for House")
+        assertEquals(housingList[0].imageLinks, mutableListOf("link1a", "link1b"))
+        assertEquals(housingList[0].startDate, startDate)
+    }
+
+    @Test
+    fun testHousingRepo2() {
+        assertEquals(housingList[1].name, "Table")
+        assertEquals(housingList[1].description, "Description for Table")
+        assertEquals(housingList[1].imageLinks, mutableListOf("link2a", "link2b", "link2c"))
+        assertEquals(housingList[1].startDate, startDate)
+    }
 }
