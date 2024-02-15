e56d19ef201da51e72b7dfd08aef75b9ddfc5743
michaelUWCS <hh.michael2000@gmail.com>
2024-02-14 21:28:09 -0500

    changed bottomNavigationItem class hasNews and badgeCount value type so that clicking on tab with news or badgecount of not 0 stop them from appearing

--- StuBnB/app/src/main/java/com/example/myapplication/screens/BottomNavigationBar.kt
+++ StuBnB/app/src/main/java/com/example/myapplication/screens/BottomNavigationBar.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import androidx.compose.foundation.layout.PaddingValues
@@ -18,12 +17,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myapplication.components.ActionButton
@@ -36,8 +31,8 @@ data class BottomNavigationItem(
     val title: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
-    val hasNews: Boolean,
-    val badgeCount: Int? = null // can have no badge
+    val hasNews: MutableState<Boolean>,
+    val badgeCount: MutableState<Int> // can have no badge
 )
 
 
@@ -48,31 +43,36 @@ fun getBottomNavigationItems(): List<BottomNavigationItem> {
             title = "Housing",
             selectedIcon = Icons.Filled.Home,
             unselectedIcon = Icons.Outlined.Home,
-            hasNews = false,
+            hasNews = mutableStateOf(false),
+            badgeCount = mutableStateOf(0)
         ),
         BottomNavigationItem(
             title = "Inventory",
             selectedIcon = Icons.Filled.Build,
             unselectedIcon = Icons.Outlined.Build,
-            hasNews = false,
+            hasNews = mutableStateOf(false),
+            badgeCount = mutableStateOf(0)
         ),
         BottomNavigationItem(
             title = "Wishlist",
             selectedIcon = Icons.Filled.Favorite,
             unselectedIcon = Icons.Outlined.Favorite,
-            hasNews = false,
+            hasNews = mutableStateOf(true), // for sprint 1 demo
+            badgeCount = mutableStateOf(0)
         ),
         BottomNavigationItem(
             title = "Inbox",
             selectedIcon = Icons.Filled.Email,
             unselectedIcon = Icons.Outlined.Email,
-            hasNews = false,
+            hasNews = mutableStateOf(false),
+            badgeCount = mutableStateOf(3) // for sprint 1 demo
         ),
         BottomNavigationItem(
             title = "Profile",
             selectedIcon = Icons.Filled.Person,
             unselectedIcon = Icons.Outlined.Person,
-            hasNews = false,
+            hasNews = mutableStateOf(false),
+            badgeCount = mutableStateOf(0)
         ),
     )
 }
@@ -110,9 +110,9 @@ fun DisplayBottomBar(){
                                 //      reference : https://www.composables.com/material3/badgedbox
                                 BadgedBox(
                                     badge = {
-                                        if (item.badgeCount != null) {
-                                            Badge { Text(text = item.badgeCount.toString()) }
-                                        } else if (item.hasNews) {
+                                        if (item.badgeCount.value != 0) {
+                                            Badge { Text(text = item.badgeCount.value.toString()) }
+                                        } else if (item.hasNews.value) {
                                             Badge()
                                         }
                                     }
@@ -132,20 +132,41 @@ fun DisplayBottomBar(){
                 }
             }
         ) {
-            innerPadding -> MainContent(selectedIndex, innerPadding)
+            innerPadding -> MainContent(selectedIndex, innerPadding, items)
             // pass in the index to jump to different tabs
         }
     }
 }
 
 @Composable
-fun MainContent(selectedItemIndex: Int, innerPadding: PaddingValues) {
+fun MainContent(selectedItemIndex: Int, innerPadding: PaddingValues, items: List<BottomNavigationItem>) {
     when (selectedItemIndex) {
-        0 -> HousingScreen()
-        1 -> InventoryScreen()
-        2 -> WishScreen()
-        3 -> InboxScreen()
-        4 -> ProfileScreen()
+        0 -> {
+            HousingScreen()
+            items[0].hasNews.value = false
+            items[0].badgeCount.value = 0
+        }
+        1 -> {
+            InventoryScreen()
+            items[1].hasNews.value = false
+            items[1].badgeCount.value = 0
+        }
+
+        2 -> {
+            WishScreen()
+            items[2].hasNews.value = false
+            items[2].badgeCount.value = 0
+        }
+        3 -> {
+            InboxScreen()
+            items[3].hasNews.value = false
+            items[3].badgeCount.value = 0
+        }
+        4 -> {
+            ProfileScreen()
+            items[4].hasNews.value = false
+            items[4].badgeCount.value = 0
+        }
     }
 }
 
