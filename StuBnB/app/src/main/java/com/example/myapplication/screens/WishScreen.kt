3d94921d1422534313e67cc52ac5badcef0bf358
michaelUWCS <hh.michael2000@gmail.com>
2024-03-13 16:43:45 -0400

    implemented WishScreen.kt

--- StuBnB/app/src/main/java/com/example/myapplication/screens/WishScreen.kt
+++ StuBnB/app/src/main/java/com/example/myapplication/screens/WishScreen.kt
@@ -1,3 +1,10 @@
 package com.example.myapplication.screens
 
+import androidx.compose.runtime.Composable
+import com.example.myapplication.models.WishList
 
+@Composable
+fun WishList(wish: WishList) {
+    HousingList(wish.housings)
+    InventoryList(wish.inventories)
+}
